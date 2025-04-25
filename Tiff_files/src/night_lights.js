7986a0cecf4246cca4dd4d58efbf1a4d501df329
Susheel Krishna <112755744+JabadeSusheelKrishna@users.noreply.github.com>
2025-04-26 01:35:04 +0530

    Update night_lights.js

--- Tiff_files/src/night_lights.js
+++ Tiff_files/src/night_lights.js
@@ -1,31 +1,32 @@
-var geometry = /* color: #0B4A8B */ee.Geometry.Polygon(
-        [[[-116.48307387522725, 37.12246623202814],
-          [-116.48307387522725, 34.98210532236753],
-          [-113.47281996897725, 34.98210532236753],
-          [-113.47281996897725, 37.12246623202814]]], null, false);
-
-// change the above coordinates to your desire location
+var geometry = 
+    /*WASHINGTON*/
+    ee.Geometry.Polygon(
+        [[[-78.49769757376221, 39.90711575123437],
+          [-78.49769757376221, 37.846134898619894],
+          [-75.65223858938721, 37.846134898619894],
+          [-75.65223858938721, 39.90711575123437]]], null, false);
 
 var startYear = 2014;
 var endYear = 2022;
 
 for (var year = startYear; year <= endYear; year++) {
-  var startDate = ee.Date.fromYMD(year, 5, 1);
-  var endDate = startDate.advance(1, 'month');
-
+  var startDate = ee.Date.fromYMD(year, 1, 1);
+  var endDate = ee.Date.fromYMD(year + 1, 1, 1);
+  
   var dataset = ee.ImageCollection('NOAA/VIIRS/DNB/MONTHLY_V1/VCMCFG')
-                  .filter(ee.Filter.date(startDate, endDate));
-
-  var nighttime = dataset.select('avg_rad').median().clip(geometry);
-
+                  .filterDate(startDate, endDate)
+                  .select('avg_rad');
+  
+  var yearlyAvg = dataset.mean().clip(geometry);
+  
   Export.image.toDrive({
-    image: nighttime,
-    description: 'NightLights_' + year,
-    folder: 'NTL_05_Las_Vegas',
+    image: yearlyAvg,
+    description: 'NTL_' + year,
+    folder: 'NTL_01_Washington',
     fileNamePrefix: 'NTL_' + year,
     region: geometry,
-    scale: 500, // approximate scale in meters
-    crs: 'EPSG:4326',
-    maxPixels: 1e13
+    scale: 500,
+    maxPixels: 1e13,
+    fileFormat: 'GeoTIFF'
   });
 }
